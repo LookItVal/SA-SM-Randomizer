6ac8f32ceeb63394469855387cd065277c3359cf
QVC <qnncecil@gmail.com>
2024-04-03 13:03:11 -0500

    first commit

---
+++ additions.py
@@ -0,0 +1,5 @@
+def main():
+  print('additions.py: main() called')
+
+if __name__ == '__main__':
+  main()
---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+
+def main():
+  print('SELECTING MESSAGE FROM THE POOL')
+
+
+if __name__ == '__main__':
+  main()
