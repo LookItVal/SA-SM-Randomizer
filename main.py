02ca503734848a4b5cd0fc42fb5dc53dcd700f5b
Quinn Valencia Cecil <86853474+LookItVal@users.noreply.github.com>
2024-06-03 11:07:18 -0500

    Update messages.csv, fix typo, and update message flags

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import datetime
 
 DEFAULT_COUNT = 10
-
+0
 def main():
   print('SELECTING MESSAGE FROM THE POOL')
   message = getMessage()
@@ -18,7 +18,7 @@ def main():
   else:
     main()
     return
-  
+    
 def getMessage():
   df = pd.read_csv('data/messages.csv')
   df = df[df['Active'] == False]
